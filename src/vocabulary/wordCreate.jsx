ca344ba41f26fd1f27a5796b89e61f9a9c2f5425
KirillovaD <sdaria@inbox.ru>
2022-10-13 11:57:00 +0300

    initial commit

---
+++ src/vocabulary/wordCreate.jsx
@@ -0,0 +1,26 @@
+//создание класса слова
+// свойства: значение, транскрипция, перевод, тема
+// метод удаления слова
+// метод редактирования слова
+// метод добавления слова
+
+
+class Word {
+  constructor(defenition, transcription, translate, theme){
+    this.defenition = defenition,
+    this.transcription = transcription,
+    this.translate = translate,
+    this.theme = theme,
+  }
+
+  static create(){
+
+  }
+  static editing(){
+
+  }
+  static delete(){
+
+  }
+
+}
